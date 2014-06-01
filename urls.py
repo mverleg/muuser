fb45fd012015537bd83f960b294cae5f1f58e25d
ursula <mark.verleg@gmail.com>
2014-06-02 00:30:20 +0200

    remove debug comments

--- urls.py
+++ urls.py
@@ -10,7 +10,7 @@ from muuser.views.reset import reset_request, reset_sent, reset_new, reset_compl
 from muuser.views.subscribe import email_unsubscribe, email_subscribe
 #from muuser.views.profile_actions import profile_actions
 #from muuser.views.settings import settings
-from muuser.views.password import password, password_done
+#from muuser.views.password import password, password_done
 
 
 pattern_list = (
@@ -18,9 +18,9 @@ pattern_list = (
 	url(r'^login/$', login, name = 'login'),
 	url(r'^logout/$', logout, name = 'logout'),
 	url(r'^register/$', register, name = 'register'),
-	#url(r'^actions/$', profile_actions, name = 'profile_actions'),
+	url(r'^actions/$', profile_actions, name = 'profile_actions'),
 	url(r'^profile/$', profile, name = 'profile_info'),
-	#url(r'^settings/$', settings, name = 'profile_settings'),
+	url(r'^settings/$', settings, name = 'profile_settings'),
 	url(r'^password/$', password, name = 'profile_password'),
 	url(r'^password/done/$', password_done, name = 'profile_password_done'),
 	
